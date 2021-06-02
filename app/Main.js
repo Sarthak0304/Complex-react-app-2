60927e980bc48a3e1edf23580dc210ae146efc0f
sarthak <sarthak.gupta0304@gmail.com>
2021-06-02 17:20:28 +0530

    first commit

---
+++ app/Main.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useReducer, useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Axios from "axios";
+Axios.defaults.baseURL = process.env.BACKENDURL || "https://complex-react-app-sarthak2.herokuapp.com";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import HomeGuest from "./components/HomeGuest";
+import About from "./components/About";
+import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
+import Profile from "./components/Profile";
+
+import { useImmerReducer } from "use-immer";
+
+import StateContext from "./StateContext";
+import DispatchContext from "./DispatchContext";
+
+function Main() {
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
+    flashMessages: [],
+    user: {
+      token: localStorage.getItem("complexappToken"),
+      username: localStorage.getItem("complexappUsername"),
+      avatar: localStorage.getItem("complexappAvatar"),
+    },
+  };
+
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "login":
+        draft.loggedIn = true;
+        draft.user = action.data;
+        return;
+      case "logout":
+        draft.loggedIn = false;
+        return;
+      case "flashMessage":
+        //{console.log("in main.js switch case");
+        draft.flashMessages.push(action.value);
+        return;
+    }
+  }
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      localStorage.setItem("complexappToken", state.user.token);
+      localStorage.setItem("complexappUsername", state.user.username);
+      localStorage.setItem("complexappAvatar", state.user.avatar);
+    } else {
+      localStorage.removeItem("complexappToken");
+      localStorage.removeItem("complexappUsername");
+      localStorage.removeItem("complexappAvatar");
+    }
+  }, [state.loggedIn]);
+  //   const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
+  //   const [flashMessages, setFLashMessages] = useState([]);
+
+  //   function addFlashMessage(msg) {
+  //     setFLashMessages((prev) => prev.concat(msg));
+  //   }
+  return (
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <BrowserRouter>
+          <FlashMessages messages={state.flashMessages} />
+          <Header />
+          <Switch>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
+            <Route path="/" exact>
+              {state.loggedIn ? <Home /> : <HomeGuest />}
+            </Route>
+            <Route path="/post/:id">
+              <ViewSinglePost />
+            </Route>
+            <Route path="/create-post">
+              <CreatePost />
+            </Route>
+            <Route path="/about-us" exact>
+              <About />
+            </Route>
+            <Route path="/terms" exact>
+              <Terms />
+            </Route>
+          </Switch>
+          <Footer />
+        </BrowserRouter>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
+  );
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"));
+
+if (module.hot) {
+  module.hot.accept();
+}
---
+++ app/components/ViewSinglePost.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import Page from "./Page";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+function ViewSinglePost() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await axios.get(`/post/${id}`);
+        setPost(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log("There was a problem");
+      }
+    }
+    fetchPost();
+  }, []);
+
+  if (isLoading)
+    return (
+      <Page title="...">
+        <div>Loading...</div>
+      </Page>
+    );
+
+  const date = new Date(post.createdDate);
+  const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+
+  return (
+    <Page title={post.title}>
+      <div className="d-flex justify-content-between">
+        <h2>{post.title}</h2>
+        <span className="pt-2">
+          <a href="#" className="text-primary mr-2" title="Edit">
+            <i className="fas fa-edit"></i>
+          </a>
+          <a className="delete-post-button text-danger" title="Delete">
+            <i className="fas fa-trash"></i>
+          </a>
+        </span>
+      </div>
+
+      <p className="text-muted small mb-4">
+        <Link to={`/profile/${post.author.username}`}>
+          <img className="avatar-tiny" src={post.author.avatar} />
+        </Link>
+        Posted by <Link to={`/profile/${post.author.username}`}> {post.author.username} </Link> on {dateFormatted}
+      </p>
+
+      <div className="body-content">{post.body}</div>
+    </Page>
+  );
+}
+
+export default ViewSinglePost;
